e94ad99d9d05f379c229fc0de584bd9007735ea4
Janus <jnus@live.com.sg>
2017-04-18 17:08:04 -0500

    Made classes

---
+++ CatnipOO.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from Keys import caesarKey
+from Keys import token
+
+class Canvas:
+
+    def __init__(self):
+
+    def pullcourses:
+
+
+    def pullassignments:
+
+
+class Caesar:
+
+    def __init__(self):
+
+
+    def pullterms:
+
+
+    def pullschedule:
+
+
+class DataEntry:
+
+    def __init__(self):
+
+
+class CheckCourse:
+
+    def checkcourses:
+
+
+class CheckTerm:
+
+    def checkterm:
+
+
+class
